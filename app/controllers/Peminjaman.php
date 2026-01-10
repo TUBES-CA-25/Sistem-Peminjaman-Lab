2fce100c4733bce76f5293be6c880f55a0c841b3
B4/13020230297/Sitti Nurhalimah <halimahdrr160@gmail.com>
2026-01-10 20:07:12 +0800

    membuat modular data_peminjaman_content.php

--- app/controllers/Peminjaman.php
+++ app/controllers/Peminjaman.php
@@ -4,17 +4,12 @@ class Peminjaman extends Controller
 {
     public function index()
     {
-        $data['active_page'] = 'peminjaman'; // For sidebar highlighting
+        $data['active_page'] = 'peminjaman'; 
 
         $this->view('components/admin_head', $data);
         $this->view('components/admin_navbar', $data);
         $this->view('components/admin_sidebar', $data);
-
-        // Load the existing content file
-        // Note: The file is in app/views/admin/data_peminjaman_content.php
-        // The view loader looks in app/views/
-        $this->view('admin/data_peminjaman_content', $data);
-
+        $this->view('admin/peminjaman/index', $data);
         $this->view('components/admin_footer', $data);
     }
 }
