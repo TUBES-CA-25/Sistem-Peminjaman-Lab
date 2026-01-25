a3d155e928a71a3011107c1d927e3d0ea1f62efe
B4/13020230297/Sitti Nurhalimah <halimahdrr160@gmail.com>
2026-01-25 23:17:42 +0800

    menyalaraskan navbar external, internal, admin

--- app/views/components/internal_navbar.php
+++ app/views/components/internal_navbar.php
@@ -1,5 +1,7 @@
 <nav class="navbar">
     <a href="<?= BASE_URL ?>/internal/booking" class="navbar-brand">
+        <img src="<?= BASE_URL ?>/public/storage/images/logo-iclabs.png" alt="Logo ICLABS" height="40" style="vertical-align: middle; margin-right: 8px;">
+        
         ICLABS <span class="admin-badge" style="background: #dcfce7; color: #059669;">Internal</span>
     </a>
 
